0689b86a8b10bc86873823d7b3040cea0814646a
Thang Chin <chinvps@ducthangchin.popcorn.com-hkgn>
2022-12-27 16:27:52 +0700

    change

--- app/server/config/database.config.js
+++ app/server/config/database.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     user: 'uwjp41cc2guf7bii',
     host: 'bhdkevvwuqmayn2gcobm-mysql.services.clever-cloud.com',
-    password: 'PAST-DATABASE-PASSWORD-HERE',
+    password: 'kNdLbnDfQargSAFo0SNw',
     database: 'bhdkevvwuqmayn2gcobm',
 }
