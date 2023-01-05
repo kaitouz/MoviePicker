55cc7b2de6e2d5fa81138dc95dd21aa18e697d1f
ducthangchin <trinhducthang201@gmail.com>
2023-01-05 04:01:04 +0300

    genre search

--- app/client/src/api/serverAPI/apiConfig.js
+++ app/client/src/api/serverAPI/apiConfig.js
@@ -1,7 +1,7 @@
 const apiConfig = {
     imgURL: (path) => `http://image.popcorn.ducthangchin.engineer/images/${path}`,
-    //baseURL: 'http://api.v1.ducthangchin.engineer'
-    baseURL: 'http://localhost:5000'
+    baseURL: 'http://api.v1.ducthangchin.engineer'
+    //baseURL: 'http://localhost:5000'
 }
 
 export default apiConfig
