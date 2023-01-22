feaaf218ef969acd7a4624239c4d199b7eab7345
jasmine150720 <jasmine150720@gmail.com>
2023-01-23 00:56:39 +0300

    bookmark test

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,6 +1,8 @@
 import './App.scss';
 import { BrowserRouter } from 'react-router-dom';
 
+import Bookmark from './components/bookmark/Bookmark';
+
 import Routes from './config/Routes';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer';
@@ -15,6 +17,8 @@ const App = () => {
           <Footer />
         </div>
       </BrowserRouter>
+
+      <Bookmark movieId='1211' category='movie' onClick={() => console.log('click')}/>
     </div>
   )
 }
